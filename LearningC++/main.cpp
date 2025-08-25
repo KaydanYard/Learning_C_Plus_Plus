d4754fb8e4a605ab774ed98236125431da0cd0f0
KaydanYard <kaydanyardleyis@gmail.com>
2025-08-25 13:09:11 -0600

    2nd activity done, working on mathematical expressions.

---
+++ LearningC++/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+int main() {
+	/*
+	int FileSize = 0;
+	double Sales = 9.99;
+	std::cout << FileSize;
+	return 0;
+	*/
+
+// First Exercise swap the Values of a and b
+	/*
+	int a = 1;
+	int b = 2;
+	int Holder = a;
+
+	 a = b;
+	 b = Holder;
+
+	std::cout << b;
+	*/
+
+// Constants
+	//const double pi = 3.14;
+	//return 0;
+
+// Mathematical Expressions
+	/*
+	int x = 10;
+	int y = x++; // x = 11, y = 10
+	int z = ++x; // x = 12, z = 12
+	std::cout << y;
+	return 0;
+	*/
+
+	// Order of operations
+		// ()
+		// * and /
+		// + and -
+
+	// 2nd Activity
+	double x = 10;
+	int y = 5;
+	double z = (x + 10) / (3 * y);
+	std::cout << z;
+	return 0;
+}
