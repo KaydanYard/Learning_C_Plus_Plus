2f8fff836d4cf0fc42cf261164f6e07e22288c79
KaydanYard <kaydanyardleyis@gmail.com>
2025-08-26 12:45:00 -0600

    Exercise 7: Create a table using cout is complete

--- LearningC++/main.cpp
+++ LearningC++/main.cpp
@@ -139,7 +139,7 @@ int main() {
 	return 0;*/
 
 // Exercise 6: Roll a dice, should generate 2 random numbers 
-	const short minValue = 1;
+	/*const short minValue = 1;
 	const short maxValue = 6;
 	srand(time(nullptr));
 
@@ -149,9 +149,21 @@ int main() {
 	cout << diceRoll1 << endl
 		 << diceRoll2;
 
-	return 0;
+	return 0;*/
 
-//// Formatting Output
-//	cout << "Spring" << "Nice" << endl
-//		 << "Summer" << "Hot";
+// Formatting Output
+	/*cout << left;
+	cout << setw(10) << "Spring" << setw(10) << "Nice"  << endl
+		 << setw(10) << "Summer" << setw(10) << "Hot";*/
+
+	/*cout << fixed << setprecision(2) <<  12.34567 << endl
+		 << 22.123456;
+
+	return 0;*/
+
+// Exercise 7: Create a table using cout
+	cout << left << setw(15) << "Course" << setw(15) << "Students" << endl
+		 << left << setw(15) << "C++" << right << setw(10) << 100 << endl
+		 << left << setw(15) << "JavaScript" << right << setw(10) << 50;
+	return 0;
 }
