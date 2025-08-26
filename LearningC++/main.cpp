9d6b3fb2833effa7cf2e58b3b42f2f7cae775777
KaydanYard <kaydanyardleyis@gmail.com>
2025-08-26 12:27:44 -0600

    Exercise 6 make a dice with 2 random values completed.

--- LearningC++/main.cpp
+++ LearningC++/main.cpp
@@ -1,11 +1,15 @@
 #define _USE_MATH_DEFINES
 
-#include <iostream>
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+#include <iostream>
 
 using namespace std; //allow you to use std without having to put it before every item that is apart of it.
 
 int main() {
+
 	/*
 	int FileSize = 0;
 	double Sales = 9.99;
@@ -13,7 +17,7 @@ int main() {
 	return 0;
 	*/
 
-// First Exercise swap the Values of a and b
+// Exercise 1: swap the Values of a and b
 	/*
 	int a = 1;
 	int b = 2;
@@ -26,8 +30,8 @@ int main() {
 	*/
 
 // Constants
-	//const double pi = 3.14;
-	//return 0;
+	/*const double pi = 3.14;
+	return 0;*/
 
 // Mathematical Expressions
 	/*
@@ -38,12 +42,12 @@ int main() {
 	return 0;
 	*/
 
-	// Order of Operations
-		// ()
-		// * and /
-		// + and -
+// Order of Operations
+	// ()
+	// * and /
+	// + and -
 
-// 2nd Activity make z = 1.3333 using Order of Operations
+// Exercise 2: make z = 1.3333 using Order of Operations
 	/*
 	double x = 10;
 	int y = 5;
@@ -59,7 +63,7 @@ int main() {
 			  << " y = " << y;
 	return 0;*/
 
-// 3rd Activity Print out Sales, State Tax, County Tax, and Total Tax
+// Exercise 3: Print out Sales, State Tax, County Tax, and Total Tax
 	/*double sales = 95000;
 
 	const double stateTaxRate = 0.04;
@@ -83,7 +87,7 @@ int main() {
 	cout << "x = " << x << ", y = " << y;
 	return 0;*/
 
-// 4th Activity Convert Fahrenheit to Celsius
+// Exercise 4: Convert Fahrenheit to Celsius
 	/*double fahrenheit;
 	
 	cout << "Enter the Fahrenheit value: ";
@@ -99,10 +103,55 @@ int main() {
 	cout << result;
 	return 0;*/
 
-// 5th Activity Calculate the Area of a Circle
-	double radius;
+// Exercise 5: Calculate the Area of a Circle
+	/*double radius;
 	cout << "Enter the Radius: ";
 	cin >> radius;
 	double area = M_PI * (radius * radius);
-	cout << area;
+	cout << area;*/
+
+// Initializing Variables
+	/*double price = 99.99;
+	float interestRate = 3.67F;
+	long fileSize = 90000L;
+	char letter = 'a';
+	bool isValid = false;
+
+	int number{};
+	cout << number;  
+	return 0;*/
+
+// Working with Numbers
+	/*int number = 0xFF;
+	cout << number;
+	return 0;*/
+
+// Narrowing
+	/*int number = 1'000'000;
+	short another{ number };
+	cout << another;
+	return 0; */
+
+// Generating Random Numbers
+	/*srand(time(0));
+	int number = rand() % 10;
+	cout << number;
+	return 0;*/
+
+// Exercise 6: Roll a dice, should generate 2 random numbers 
+	const short minValue = 1;
+	const short maxValue = 6;
+	srand(time(nullptr));
+
+	short diceRoll1 = (rand() % (maxValue - minValue + 1)) + minValue;
+	short diceRoll2 = (rand() % (maxValue - minValue + 1)) + minValue;
+	
+	cout << diceRoll1 << endl
+		 << diceRoll2;
+
+	return 0;
+
+//// Formatting Output
+//	cout << "Spring" << "Nice" << endl
+//		 << "Summer" << "Hot";
 }
