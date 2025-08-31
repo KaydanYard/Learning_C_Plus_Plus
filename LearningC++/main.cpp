a7b0a12ba09de3444638546bc17ba314b116fb94
KaydanYard <kaydanyardleyis@gmail.com>
2025-08-31 11:27:28 -0600

    Exercise 13 Create a basic calculator

--- LearningC++/main.cpp
+++ LearningC++/main.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 int main() {
 // The Switch Statement
-	cout << "1- Create account" << endl
+	/*cout << "1- Create account" << endl
 		 << "2- Change password" << endl
 		 << "3- Quit" << endl
 		 << "Select an option: ";
@@ -28,7 +28,46 @@ int main() {
 			break;
 		default:
 			cout << "Goodbye!";
+	}*/
+
+// Exercise 13 create a basic calculator
+	int var1;
+	int var2;
+	int op3rator;
+	string output;
+
+	cout << "Enter the first value: ";
+	cin >> var1;
+	
+	cout << "Pick one of the following" << endl
+		 << "1- Multiply" << endl
+		 << "2- Divide" << endl
+		 << "3- Add" << endl
+		 << "4- Subtract" << endl;
+	cin >> op3rator;
+	
+	cout << "Enter the second value: ";
+	cin >> var2;
+
+	switch (op3rator) {
+		case 1:
+			output = var1 * var2;
+			break;
+		case 2:
+			output = var1 / var2;
+			break;
+		case 3:
+			output = var1 + var2;
+			break;
+		case 4:
+			output = var1 - var2;
+			break;
+		default:
+			output = "NaN";
+			cout << "I don't understand" << endl;
 	}
+
+	cout << "Output: " << output;
 
 	return 0;
 }
