ba9e8bb927065cd9a3600ea6bc78bde5a6d0cf75
KaydanYard <kaydanyardleyis@gmail.com>
2025-08-31 11:08:25 -0600

    Switch Statements

--- LearningC++/main.cpp
+++ LearningC++/main.cpp
@@ -10,21 +10,25 @@
 using namespace std;
 
 int main() {
-// The Conditional Operator
-	/*int sales = 11'000;
-	double commission = (sales > 10'000) ? 0.1 : 0.05;
+// The Switch Statement
+	cout << "1- Create account" << endl
+		 << "2- Change password" << endl
+		 << "3- Quit" << endl
+		 << "Select an option: ";
 
-	cout << commission;*/
+	int input;
+	cin >> input;
 
-// Exercise 12: get two numbers, store the larger in a seperate variable, print the larger
-	cout << "Enter two values: ";
-
-	int num1;
-	int num2;
-	cin >> num1 >> num2;
-	
-	int bigNum = (num1 > num2) ? num1 : num2;
-	cout << "Big Num: " << bigNum;
+	switch (input) {
+		case 1: 
+			cout << "You selected 1";
+			break;
+		case 2:
+			cout << "You selected 2";
+			break;
+		default:
+			cout << "Goodbye!";
+	}
 
 	return 0;
 }
