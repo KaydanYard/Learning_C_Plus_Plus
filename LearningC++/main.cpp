f624ffa74e0e6452577d9a1b84c6962ecd49752b
KaydanYard <kaydanyardleyis@gmail.com>
2025-09-03 18:16:46 -0600

    Do While Loops

--- LearningC++/main.cpp
+++ LearningC++/main.cpp
@@ -10,36 +10,24 @@
 using namespace std;
 
 int main() {
-// While Loops
-	/*int i = 1;
-	while (i <= 5) {
-		cout << i << endl;
-		i++;
-	}*/
+// Do-While Loops
+	/*
+	// While Loop
+	while (...) {
 
-	/*int number = 0;
-	while (number < 1 || number > 5) {
+	}
+	
+	// Do While Loop
+	do {
+
+	} while (...);*/
+
+	int number;
+
+	do {
 		cout << "Number: ";
 		cin >> number;
-		if (number < 1 || number > 5) {
-			cout << "Invalid Number, enter a number from 1 to 5" << endl;
-		}
-	}*/
-
-// Exercise 16: Ask for the secret number
-	int secretNumber = 21;
-	int guess = 0;
-
-	while (guess != secretNumber) {
-		cout << "Guess the secret number: ";
-		cin >> guess;
-		if (secretNumber == guess) {
-			cout << "That's correct" << endl;
-		}
-		else {
-			cout << "Try Again!" << endl;
-		}
-	}
+	} while (number < 1 || number > 5);
 
 	return 0;
 }
