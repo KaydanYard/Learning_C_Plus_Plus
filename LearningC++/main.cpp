1e3d7c2a39448898b32457bcd5913f3f46ef24fc
KaydanYard <kaydanyardleyis@gmail.com>
2025-09-07 12:50:42 -0600

    Exercise 18: Receive 2 numbers and return the biggest number

--- LearningC++/main.cpp
+++ LearningC++/main.cpp
@@ -8,22 +8,37 @@
 #include <string>
 
 using namespace std;
-// Defining and Calling Functions
+// Exercise 18: Receive 2 numbers and return the biggest number
 
-void greet(string name) {
-	cout << "Hello " << name << endl;
-}
+//void greet(string name) {
+//	cout << "Hello " << name << endl;
+//}
+//
+//string fullName(string firstName, string lastName) {
+//	// Concatenating (combining)
+//	return firstName + " " + lastName;
+//}
 
-string fullName(string firstName, string lastName) {
-	// Concatenating (combining)
-	return firstName + " " + lastName;
+int compare(int intOne, int intTwo) {
+	if (intOne > intTwo) {
+		return intOne;
+	} 
+	else if (intOne < intTwo) {
+		return intTwo;
+	}
+	else {
+		return 0;
+	}
 }
 
 int main() {
-// Calling - invoking - executing
-	greet(fullName("Kaydan", "Yardley"));
+//// Calling - invoking - executing
+//	greet(fullName("Kaydan", "Yardley"));
+//
+//	cout << "Done";
+//
 
-	cout << "Done";
+	cout << compare(3, 23);
 
 	return 0;
 }
