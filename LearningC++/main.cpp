62319dc257baf8f70b9a7728cca9e02801cd2a2e
KaydanYard <kaydanyardleyis@gmail.com>
2025-09-09 19:54:22 -0600

    Exercise 19: Create a Linear Search Algorithm

--- LearningC++/main.cpp
+++ LearningC++/main.cpp
@@ -3,12 +3,28 @@
 
 using namespace std;
 
+// Exercise 19: Create a Linear Search Algorithm (very basic shit)
+
+int findTargetValue(int* values, int size, int targetValue) {
+	for (int i = 0; i < size; i++) {
+		if (targetValue == values[i]) {
+			cout << targetValue << " and " << values[i] << " match" << endl;
+			return targetValue;
+		}
+		else {
+			cout << "Not a match" << endl;
+		}
+	}
+
+	cout << "Couldn't find a single match" << endl;
+	return -1;
+}
+
 int main() {
-	int values[3] =  { 10, 20, 30 };
+	int values[] =  { 10, 20, 30 };
+	int targetValue = 30;
 
-	// C++: structured binding
-	// Python: unpacking
-	auto [x, y, z] = values;
-
+	findTargetValue(values, sizeof(values) / sizeof(int), targetValue);
+	
 	return 0;
 }
