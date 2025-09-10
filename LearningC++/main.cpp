5b074a19867f81da32440e8f99c08556c63bce28
KaydanYard <kaydanyardleyis@gmail.com>
2025-09-09 19:59:04 -0600

    Exercise 19: Create a Linear Search Algorithm

--- LearningC++/main.cpp
+++ LearningC++/main.cpp
@@ -5,11 +5,12 @@ using namespace std;
 
 // Exercise 19: Create a Linear Search Algorithm (very basic shit)
 
-int findTargetValue(int* values, int size, int targetValue) {
+					// size_t is better than int because it can hold more
+int findTargetValue(int* values, size_t size, int targetValue) {
 	for (int i = 0; i < size; i++) {
 		if (targetValue == values[i]) {
-			cout << targetValue << " and " << values[i] << " match" << endl;
-			return targetValue;
+			cout << "Match found" << endl;
+			return values[i];
 		}
 		else {
 			cout << "Not a match" << endl;
@@ -24,7 +25,7 @@ int main() {
 	int values[] =  { 10, 20, 30 };
 	int targetValue = 30;
 
-	findTargetValue(values, sizeof(values) / sizeof(int), targetValue);
+	cout << findTargetValue(values, sizeof(values) / sizeof(int), targetValue);
 	
 	return 0;
 }
