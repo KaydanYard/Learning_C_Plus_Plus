3a9d1ccc4c60144be6b049883681c3d30a9c7654
KaydanYard <kaydanyardleyis@gmail.com>
2025-10-06 18:26:07 -0600

    Dynamically Resizing an Array

--- LearningC++/main.cpp
+++ LearningC++/main.cpp
@@ -3,14 +3,41 @@
 
 using namespace std;
 
-// Dynamic Memory Allocation
+// Dynamically Resizing an Array
 
 int main() {
+	int capacity = 5;
+	int* numbers = new int[capacity];
+	int entries = 0;
+	
+	while (true) {
+		cout << "Number: ";
+		cin >> numbers[entries];
+		if (cin.fail()) 
+			break;
+		entries++;
 
-	// Heap (Free Store)
-	int* numbers = new int[10];
+		// The following already exists as a Vector
+		if (entries == capacity) {
+			capacity *=2;
+
+			// Create a temp array (twice the size)
+			int* temp = new int[capacity];
+
+			// Copy all the elements
+			for (int i = 0; i < entries; i++) 
+				temp[i] = numbers[i];
+		
+			// Have "numbers" pointer point to the new array
+			delete[] numbers;
+			numbers = temp;
+		}
+	}
+
+	for (int i = 0; i < entries; i++)
+		cout << numbers[i] << endl;
+
 	delete[] numbers;
-	numbers = nullptr;
 
 	return 0;
 }
