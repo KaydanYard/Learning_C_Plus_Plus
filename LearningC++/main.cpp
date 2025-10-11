1707d6ae6b71f589f93f6304a941dde3edba9b72
KaydanYard <kaydanyardleyis@gmail.com>
2025-10-11 17:40:28 -0600

    Smart Pointers: Working with Unique Pointers

--- LearningC++/main.cpp
+++ LearningC++/main.cpp
@@ -1,43 +1,16 @@
 #include <iostream>
 #include <string>
+#include <memory>
 
 using namespace std;
 
-// Dynamically Resizing an Array
+// Smart Pointers: Working with Unique Pointers
 
 int main() {
-	int capacity = 5;
-	int* numbers = new int[capacity];
-	int entries = 0;
-	
-	while (true) {
-		cout << "Number: ";
-		cin >> numbers[entries];
-		if (cin.fail()) 
-			break;
-		entries++;
-
-		// The following already exists as a Vector
-		if (entries == capacity) {
-			capacity *=2;
-
-			// Create a temp array (twice the size)
-			int* temp = new int[capacity];
-
-			// Copy all the elements
-			for (int i = 0; i < entries; i++) 
-				temp[i] = numbers[i];
-		
-			// Have "numbers" pointer point to the new array
-			delete[] numbers;
-			numbers = temp;
-		}
-	}
-
-	for (int i = 0; i < entries; i++)
-		cout << numbers[i] << endl;
-
-	delete[] numbers;
+	auto numbers = make_unique<int[]>(10);
+	auto y = make_unique<int>();
+	numbers[0] = 10;
+	cout << numbers[0];
 
 	return 0;
 }
